07419b475234e5283675e39dec8594ac6d584495
Chan <kcjeon@epyrus.com>
2023-10-18 10:38:30 +0900

    2023.10.18 URL에 변수를 넣을 수 있다 /:변수명 실제 사용될 URL이 변수로 인식되지 않도록 get함수를 변수를 사용한 get함수 위에 사용해야 한다

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,5 @@
 export const trending = (req, res) => res.send("Home");
-export const watch = (req, res) => res.send("video watch");
-export const edit = (req, res) => res.send("video watch");
+export const see = (req, res) => res.send(`video watch ${req.params.id}`);
+export const edit = (req, res) => res.send("video edit");
+export const deleteVideo = (req, res) => res.send("video delete");
+export const upload = (req, res) => res.send("video upload");
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,7 +1,11 @@
 import express from "express";
+import { see, edit, upload, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
-const handleWatchVideo = (req, res) => res.send("Video Watch");
-videoRouter.get("/watch", handleWatchVideo);
-videoRouter.get("/edit", (req, res) => res.send("Video Edit"));
+
+videoRouter.get("/upload", upload); //upload가 변수가 되지 않게 :id보다 위에 위치
+videoRouter.get("/:id", see);
+videoRouter.get("/:id/edit", edit);
+videoRouter.get("/:id/delete", deleteVideo);
+
 export default videoRouter;
