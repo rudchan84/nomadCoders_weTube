97e3e630eb43fffa62c43fc75b94fe2a8a872822
Chan <kcjeon@epyrus.com>
2023-10-25 15:21:02 +0900

    2023.10.25 #5.7 Conditionals : if문으로 제어하기

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,10 @@
-export const trending = (req, res) => res.render("home"); //home.pug를 유저에게 보여준다
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
-export const deleteVideo = (req, res) => res.send("video delete");
-export const upload = (req, res) => res.send("video upload");
+const fakeUser = {
+  username: "Nico",
+  loggedIn: true,
+};
+
+export const trending = (req, res) => res.render("home", { pageTitle: "Home", userPotato: fakeUser }); //home.pug를 유저에게 보여준다
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
+export const deleteVideo = (req, res) => res.send("video delete", { pageTitle: "Video Delete" });
+export const upload = (req, res) => res.send("video upload", { pageTitle: "Video Upload" });
