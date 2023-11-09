a4d09472d2760f868773ee431abea6b2007690ee
Chan <kcjeon@epyrus.com>
2023-11-09 17:14:25 +0900

    2023.11.09 array를 받아서 각 데이터에 특정 작업을 할 때 사용 each potato in videos https://pugjs.org/language/iteration.html
    mixin은 data를 받을 수 있는 partial(include) 이다
    html 모듈을 만들어 재사용할 수 있게끔 만들어 줌

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,12 @@
-const fakeUser = {
-  username: "Nico",
-  loggedIn: true,
+export const trending = (req, res) => {
+  const videos = [
+    { title: "First Video", rating: 5, comments: 12, createAt: "22 minutes ago", views: 95, id: 1 },
+    { title: "Second Video", rating: 4, comments: 3, createAt: "7 minutes ago", views: 23, id: 2 },
+    { title: "Third Video", rating: 3, comments: 1, createAt: "1 minutes ago", views: 2, id: 3 },
+  ];
+  return res.render("home", { pageTitle: "Home", videos: videos }); //home.pug를 유저에게 보여준다
+  //return res.render("home", { pageTitle: "Home", videos }); 이렇게 줄일 수 있다
 };
-
-export const trending = (req, res) => res.render("home", { pageTitle: "Home", userPotato: fakeUser }); //home.pug를 유저에게 보여준다
 export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const deleteVideo = (req, res) => res.send("video delete", { pageTitle: "Video Delete" });
