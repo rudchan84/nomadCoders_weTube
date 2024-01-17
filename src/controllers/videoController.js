b0037629ca01250ea7ca96dea6649971865b6547
Chan <kcjeon@epyrus.com>
2024-01-17 16:22:36 +0900

    2024.01.17 #6.26 Search part One 라우터로 지정한 :id -> req.params pug파일에서 input으로 받은 내용 -> req.body(form이 POST일 때) pug파일에서 input으로 받은 url내용 -> req.query (form이 GET일 때) #6.27 Search part Two mongoDB 정규식으로 검색 docs.mongodb.com/manual/reference/operator/query/regex

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,13 @@
 import Video /*, { makeHashtags }*/ from "../models/video";
 
+//HOME
 export const home = async (req, res) => {
-  const videos = await Video.find({});
+  const videos = await Video.find({}).sort({ createdAt: "desc" });
   //console.log("videos:", videos);
   return res.render("home", { pageTitle: "Home", videos }); //home.pug를 유저에게 보여준다
 };
+
+//WATCH
 export const watch = async (req, res) => {
   console.log(req.params); //url에 id변수가 있으니 params를 찍어볼 수 있다
   const { id } = req.params; //ES6 최신문법(아래와같음)
@@ -16,6 +19,7 @@ export const watch = async (req, res) => {
   return res.render("watch", { pageTitle: videoInfo.title, videoInfo });
 };
 
+//EDIT
 export const getEdit = async (req, res) => {
   const { id } = req.params;
   const videoInfo = await Video.findById(id);
@@ -40,12 +44,14 @@ export const postEdit = async (req, res) => {
   return res.redirect(`/video/${id}`);
 };
 
+//DELETE
 export const deleteVideo = async (req, res) => {
   const { id } = req.params;
   await Video.findByIdAndDelete(id);
   return res.redirect("/");
 };
 
+//UPLOAD
 export const getUpload = (req, res) => res.render("upload", { pageTitle: "Video Upload" });
 export const postUpload = async (req, res) => {
   //console.log(req.body);
@@ -61,7 +67,7 @@ export const postUpload = async (req, res) => {
     await video.save();
   } catch (error) {
     // console.log(error);
-    return res.render("upload", { pageTitle: "Video Upload", errorMessage: error._message });
+    return res.render("upload", { pageTitle: "Video Upload", errorMessage: error.errors.description });
   }
   /*
   위의 new object 생성 후 save하는 부분을 아래와 같이 사용할 수 있다
@@ -74,4 +80,17 @@ export const postUpload = async (req, res) => {
    */
   return res.redirect("/");
   //return res.render("upload", { pageTitle: "Video Upload" });
+};
+
+//SEARCH
+export const search = async (req, res) => {
+  const { searchTitle } = req.query;
+  let videos = [];
+  if (searchTitle) {
+    videos = await Video.find({ title: { $regex: new RegExp(searchTitle, "i") } }).sort({ createdAt: "desc" });
+    /*정규식
+    new RegExp(`^${searchTitle}`, "i") 검색어로 시작하는 것만
+    new RegExp(`${searchTitle}$`, "i") 검색어로 끝나는 것만 */
+  }
+  return res.render("search", { pageTitle: "Video Search", videos });
 };
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { home } from "../controllers/videoController";
+import { home, search } from "../controllers/videoController";
 import { join } from "../controllers/userController";
 
 const globalRouter = express.Router();
 
 globalRouter.get("/", home);
 globalRouter.get("/join", join);
+globalRouter.get("/search", search);
 
 export default globalRouter;
